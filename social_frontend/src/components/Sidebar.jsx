1f195e5ce646dcd12e4a575f44f6ddc9de20ab55
stens0n <astenson010@gmail.com>
2022-02-02 15:13:24 -0500

    3rd commit

---
+++ social_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Siderbar = () => {
+  return (
+    <div>
+        Sidebar
+    </div>
+    )
+};
+
+export default Siderbar;
---
+++ social_frontend/src/container/Pins.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Pins = () => {
+  return (
+  <div>
+      Pins
+  </div>
+  )
+};
+
+
+export default Pins
